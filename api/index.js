83de8e0e2fc23c44cb7de7d38d469e5b33e7b4eb
Sucipto1925 <sucipto1925@gmail.com>
2026-08-26 21:51:52 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
-const router = require('./routes/index')
+const router = require('../routes/index')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const fileUpload = require('express-fileupload')
